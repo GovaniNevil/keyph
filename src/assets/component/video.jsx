6b15cec2ef5db8b8942151497e1a7437fc552f1e
MR. Golu <88421428+govaninevil@users.noreply.github.com>
2024-10-01 10:46:18 +0530

    main

--- src/assets/component/video.jsx
+++ src/assets/component/video.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import introvideo from '../images/KEYPH_SOLAR_SOLUTIONN.mp4'
+import introvideo from '../images/KEYPH_SOLAR_SOLUTIONN.mp4';
 
-const Video = () => {
+const Video = ({ onVideoLoad }) => {
   return (
     <div>
-        <video src={introvideo} muted autoPlay loop className='mt-20 sm:mt-0'></video>
+      <video 
+        src={introvideo} 
+        muted 
+        autoPlay 
+        loop 
+        onLoadedData={onVideoLoad} // This will trigger the callback once the video is loaded
+        className='mt-20 sm:mt-0'>
+      </video>
     </div>
   )
 }
 
-export default Video
+export default Video;
--- src/assets/pages/home.jsx
+++ src/assets/pages/home.jsx
@@ -1,30 +1,42 @@
-import React from 'react'
-import Header from '../component/header'
-import Video from '../component/video'
-import Solution from '../component/solution'
-import Aboutintro from '../component/aboutintro'
-import Whyus from '../component/whyus'
-import Vision from '../component/vision'
-import Process from '../component/process'
-import Partner from '../component/partner'
-import Banefits from '../component/banefits'
-import Freedom from '../component/freedom'
-import Footer from '../component/footer'
+import React, { useState } from 'react';
+import Header from '../component/header';
+import Video from '../component/video';
+import Solution from '../component/solution';
+import Aboutintro from '../component/aboutintro';
+import Whyus from '../component/whyus';
+import Vision from '../component/vision';
+import Process from '../component/process';
+import Partner from '../component/partner';
+import Banefits from '../component/banefits';
+import Freedom from '../component/freedom';
+import Footer from '../component/footer';
 
 const Home = () => {
+  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
+
+  // Callback function that will be triggered when the video is loaded
+  const handleVideoLoad = () => {
+    setIsVideoLoaded(true); // Set the state to true after the video is loaded
+  };
+
   return (
     <div>
       <Header />
-      <Video />
-      <Aboutintro />
-      <Whyus />
-      <Partner />
-      <Vision />
-      <Process />
-      <Footer />
-
+      <Video onVideoLoad={handleVideoLoad} /> {/* Pass the callback function */}
+      
+      {/* Conditionally render the other components only when the video is loaded */}
+      {isVideoLoaded && (
+        <>
+          <Aboutintro />
+          <Whyus />
+          <Partner />
+          <Vision />
+          <Process />
+          <Footer />
+        </>
+      )}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
